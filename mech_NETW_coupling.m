6ea273626e02f28f3ff05c9104e2a1855904868e
David <david.snee@northumbria.ac.uk>
2018-02-08 17:23:04 +0000

    first commit

---
+++ mech_NETW_coupling.m
@@ -0,0 +1,74 @@
+%% Contains all of the coupling coefficients for the linear ODE. 
+
+function el = mech_NETW_coupling(r1,r2,s1,s2,par,i1,i2)
+omega0=par(1);
+A=par(2);
+f=par(3);
+coef=-(omega0^2+A*f);
+
+if i1==1 || i1==2
+    if (r1-r2==-1 && s1-s2==0) || (r1-r2==1 && s1-s2==0) || (r1-r2==0 && s1-s2==-1) || (r1-r2==0 && s1-s2==1)
+      el = f;
+    elseif r1-r2==0 && s1-s2==0
+      el = coef;
+    else
+      el = 0;
+    end  
+elseif i1==3
+    if (r1-r2==0 && s1-s2==1) || (r1-r2==0 && s1-s2==-1) 
+      el = f;
+    elseif (r1-r2==-1 && s1-s2==0 && mod(i2,2)==1) || (r1-r2==1 && s1-s2==0 && mod(i2,2)==1)
+      el = -f/2; 
+    elseif r1-r2==-1 && s1-s2==0 && mod(i2,2)==0
+      el = sqrt(3)*f/2;
+    elseif r2-r1==-1 && s2-s1==0
+      el = -sqrt(3)*f/2;
+    elseif r1-r2==0 && s1-s2==0
+      el = coef;
+    else
+      el = 0;
+    end
+elseif i1==4
+    if (r1-r2==0 && s1-s2==1) || (r1-r2==0 && s1-s2==-1) 
+      el = f;
+    elseif (r1-r2==-1 && s1-s2==0 && mod(i2,2)==0) || (r1-r2==1 && s1-s2==0 && mod(i2,2)==0)
+      el = -f/2; 
+    elseif r2-r1==-1 && s2-s1==0
+      el = sqrt(3)*f/2;
+    elseif r1-r2==-1 && s1-s2==0 && mod(i2,2)==1
+      el = -sqrt(3)*f/2;
+    elseif r1-r2==0 && s1-s2==0
+      el = coef;
+    else
+      el = 0;
+    end
+elseif i1==5
+    if (r1-r2==0 && s1-s2==1) || (r1-r2==0 && s1-s2==-1) 
+      el = f;
+    elseif (r1-r2==-1 && s1-s2==0 && mod(i2,2)==1) || (r1-r2==1 && s1-s2==0 && mod(i2,2)==1)
+      el = -f/2; 
+    elseif r2-r1==-1 && s2-s1==0
+      el = sqrt(3)*f/2;
+    elseif r1-r2==-1 && s1-s2==0 && mod(i2,2)==0
+      el = -sqrt(3)*f/2;
+    elseif r1-r2==0 && s1-s2==0
+      el = coef;
+    else
+      el = 0;
+    end
+elseif i1==6
+    if (r1-r2==0 && s1-s2==1) || (r1-r2==0 && s1-s2==-1) 
+      el = f;
+    elseif (r1-r2==-1 && s1-s2==0 && mod(i2,2)==0) || (r1-r2==1 && s1-s2==0 && mod(i2,2)==0)
+      el = -f/2; 
+    elseif r1-r2==-1 && s1-s2==0 && mod(i2,2)==1
+      el = sqrt(3)*f/2;
+    elseif r2-r1==-1 && s2-s1==0
+      el = -sqrt(3)*f/2;
+    elseif r1-r2==0 && s1-s2==0
+      el = coef;
+    else
+      el = 0;
+    end
+end
+end
