b02acb79c98155a5e2a64a508f7597cebfe72157
David <david.snee@northumbria.ac.uk>
2018-02-08 17:22:16 +0000

    first commit

---
+++ mech_NETW_matrix_periodic_coupling.m
@@ -0,0 +1,77 @@
+%% Generates the matrix defining the linear RHS of the equations.
+
+function rhs = mech_NETW_matrix_periodic_coupling(rsx0,rsy0,rsx1,rsy1,rsx2,rsy2,bonds,par,Ns)
+N1 = size(rsx0,1);
+N2 = size(rsy0,1);
+N3 = size(rsx1,1);
+N4 = size(rsy1,1);
+N5 = size(rsx2,1);
+N6 = size(rsy2,1);
+N = [N1 N2 N3 N4 N5 N6]; TN=sum(N);
+Nb = size(bonds,1);
+MAT = sparse(TN,TN);
+for i = 1:Nb
+    M1=[]; M2=[];
+    k=find(bonds(i,:));
+    i1=k(1);
+    i2=k(2);
+    b1=bonds(i,i1);
+    b2=bonds(i,i2);
+    if i1==1
+        rs1=rsx0;
+        M1=0;
+    elseif i1==2
+        rs1=rsy0;
+        M1=N1;
+    elseif i1==3
+        rs1=rsx1;
+        M1=sum(N(1:2));
+    elseif i1==4
+        rs1=rsy1;
+        M1=sum(N(1:3));
+    elseif i1==5
+        rs1=rsx2;
+        M1=sum(N(1:4));
+    elseif i1==6
+        rs1=rsy2;
+        M1=sum(N(1:5));
+    end
+    if i2==7
+        rs2=rsx0;
+        M2=0;
+    elseif i2==8
+        rs2=rsy0;
+        M2=N1;
+    elseif i2==9
+        rs2=rsx1;
+        M2=sum(N(1:2));
+    elseif i2==10
+        rs2=rsy1;
+        M2=sum(N(1:3));
+    elseif i2==11
+        rs2=rsx2;
+        M2=sum(N(1:4));
+    elseif i2==12
+        rs2=rsy2;
+        M2=sum(N(1:5));
+    end
+    RS1=rs1(b1,:); RS2=rs2(b2,:);
+    r1 = RS1(1); s1 = RS1(2); r2 = RS2(1); s2 = RS2(2);
+    
+    F1=find(ismember(rs1(:,1),r1)==1);
+    F2=find(ismember(rs1(:,2),s1)==1);
+    C1=intersect(F1,F2);
+    F3=find(ismember(rs2(:,1),r2)==1);
+    F4=find(ismember(rs2(:,2),s2)==1);
+    C2=intersect(F3,F4);
+    
+    test1(i,1)=b1;
+    test1(i,2)=b2;
+    test2(i,1)=C1;
+    test2(i,2)=C2;
+    
+    MAT(M1+b1,M2+b2) = mech_NETW_coupling_periodic_domain(r1,r2,s1,s2,par,i1,i2,Ns);
+end
+
+rhs = MAT; 
+end
